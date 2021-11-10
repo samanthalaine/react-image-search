4b5dcd10a464ed8409090bbd390b06bcb88c6b62
Samantha Laine <samanthalaine8@gmail.com>
2021-11-10 18:07:14 -0500

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react';
+
+
 function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [term, setTerm] = useState('')
+
+  // useEffect(() =>{
+  //   fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+  //   .then(res => res.json())
+  //   .then(data=>console.log(data))
+  //   .catch(err => console.error(err))
+  // }, [])
+
+  useEffect(() => {
+    try{
+    const fetchImages = async () => {
+      const res = await fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchImages()
+  } catch (err) {
+    console.log(err)
+  }
+  },[])
+
+  
+
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
       <div className="px-6 py-3">
       <div className="font-bold text-blue-700 text-xl mb-2">
