7c918c92580589a86908a42f34059768880ec068
Samantha Laine <samanthalaine8@gmail.com>
2021-11-10 16:47:06 -0500

    created card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-
-
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl">Hello World</h1>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-3">
+      <div className="font-bold text-blue-700 text-xl mb-2">
+        Photo by Samantha Laine
+      </div>
+      <ul>
+        <li>
+          <strong>Views: </strong>
+          4000
+        </li>
+        <li>
+          <strong>Downloads: </strong>
+          4000
+        </li>
+        <li>
+          <strong>Likes: </strong>
+          4000
+        </li>
+      </ul>
+      </div>
+      <div className="px-6 py-3">
+        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          #tag1
+        </span>
+      </div>
     </div>
   );
 }
