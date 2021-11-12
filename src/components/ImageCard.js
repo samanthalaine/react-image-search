01fa5c908a5403cd022688e597d4872d4fa82870
Samantha Laine <samanthalaine8@gmail.com>
2021-11-11 19:09:02 -0500

    rendered photo data

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,38 +1,35 @@
 import React from 'react'
 
 function ImageCard({image}) {
+  const tags = image.tags.split(',')
+
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={image.webformatURL} alt="" className="w-full"/>
+      <img src={image.webformatURL} alt={image.webformatURL} className="w-full"/>
       <div className="px-6 py-3">
       <div className="font-bold text-blue-700 text-xl mb-2">
-        Photo by Samantha Laine
+        Photo by {image.user}
       </div>
       <ul>
         <li>
           <strong>Views: </strong>
-          4000
+          {image.views}
         </li>
         <li>
           <strong>Downloads: </strong>
-          4000
+          {image.downloads}
         </li>
         <li>
           <strong>Likes: </strong>
-          4000
+          {image.likes}
         </li>
       </ul>
       </div>
       <div className="px-6 py-3">
-        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
-          #tag1
-        </span>
+        {tags.map((tag) =>(<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          #{tag}
+        </span>))}
+       
       </div>
     </div>
     )
